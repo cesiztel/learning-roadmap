7fe9b1e2cfddbc51e1f46ec6df0f59e122d232ec
César Izquierdo Tello <Cesar.IzquierdoTello@parkmobile.com>
2019-10-14 10:26:10 +0200

    C#: Study C# program structure while I take notes about how to write a simple Stack data structure

---
+++ programminglanguages/seesharp/ProgramStructure/Stack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ProgramStructure
+{
+    /*
+        Here I want to design a Stack taking
+        advantage of the object type to make the
+        Stack to store all kind of data types.
+        This is the skeleton that I can write first
+        besides the implementation. 
+
+        - Which operations I need to implement in my Stack?
+        - How a Stack works? See the related notes in datastructures.
+     */
+    public class Stack
+    {
+        public void Push(object data)
+        {
+            // put entry on the top
+        }
+
+        public object Pop()
+        {
+            // return object
+        }
+    }
+}
