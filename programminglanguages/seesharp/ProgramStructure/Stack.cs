4b46119bae849106a2e648629f1f176801562831
César Izquierdo Tello <Cesar.IzquierdoTello@parkmobile.com>
2019-10-17 10:14:09 +0200

    Study some simple types in C#: enums and structus

--- programminglanguages/seesharp/ProgramStructure/Stack.cs
+++ programminglanguages/seesharp/ProgramStructure/Stack.cs
@@ -11,17 +11,43 @@ namespace ProgramStructure
 
         - Which operations I need to implement in my Stack?
         - How a Stack works? See the related notes in datastructures.
+        - How to implement a Stack. There are many ways to implement a stack.
+        In this implementation of the stack the top element holds the instance
+        of the next element. So when the top wants to pop, the instance of the
+        next element take the place of top overriding the memory with the next
+        element and the top is returned.
      */
     public class Stack
     {
+        Entry top;
         public void Push(object data)
         {
-            // put entry on the top
+            top = new Entry(top, data);
         }
 
         public object Pop()
         {
-            // return object
+            if (top == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            object result = top.data;
+            top = top.next;
+
+            return result;
+        }
+
+        class Entry 
+        {
+            public Entry next;
+            public object data;
+
+            public Entry(Entry next, object data)
+            {
+                this.next = next;
+                this.data = data;
+            }
         }
     }
 }
