ec4f011ea626a8c36b08390d423d71d069d46372
Cesar Izquierdo Tello <cesar.izquierdotello@parkmobile.com>
2020-02-17 09:43:29 +0100

    feat: create new thread with Thread class

---
+++ programminglanguages/seesharp/Threading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+    class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(0);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+
+            t.Join();
+        }
+    }
+}
