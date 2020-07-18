61d4f0072b637ec222c386e763d537fc8dc469db
Cesar Izquierdo Tello <cesiztel@gmail.com>
2020-07-19 00:00:03 +0200

    Add DTO showcase and NSwagger experimentation

--- asp/TodoApi/Models/TodoItemDTO.cs
+++ asp/TodoApi/Models/TodoItemDTO.cs
@@ -1,13 +1,11 @@
 namespace TodoApi.Models 
 {
-    public class TodoItem
+    public class TodoItemDTO
     {
         public long Id { get; set; }
 
         public string Name { get; set; }
 
         public bool IsComplete { get; set; }
-
-        public string Secret { get; set; }
     }
 }
