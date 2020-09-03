d3b6ccb728fda8a849eb1e497e5ba8b1bcb7a248
Cesar Izquierdo Tello <cesar.izquierdotello@park-now.com>
2020-09-03 10:25:19 +0200

    Add project where I can put all my coding exercises

---
+++ 1_coding_exercises/src/CodingExercises.Test/SumArrayTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CodingExercises.Test
+{
+    public class SumArrayTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ 1_coding_exercises/src/CodingExercises/Basics/SumArray.cs
@@ -0,0 +1,10 @@
+namespace CodingExercises.Basics
+{
+    public static class SumArray
+    {
+        public static int Sum(int[] inputArray)
+        {
+            return 0;
+        }
+    }
+}
