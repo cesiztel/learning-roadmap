87d69038d43a994fab1d57262e1b2bbaa01761c9
Cesar Izquierdo Tello <cesar.izquierdotello@park-now.com>
2020-09-04 08:51:22 +0200

    feat: add sum exercise with tests and description of basic coding with arrays

--- 1_coding_exercises/src/CodingExercises.Test/SumArrayTests.cs
+++ 1_coding_exercises/src/CodingExercises.Test/SumArrayTests.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
+using CodingExercises.Basics;
 
 namespace CodingExercises.Test
 {
     public class SumArrayTests
     {
         [Fact]
-        public void Test1()
+        public void GivenACorrectArrayTheSumIsCorrectTest()
         {
-            Assert.True(true);
+            int expectedSumResult = 5;
+
+            int[] myArray = new int[] {1, 1, 1, 1, 1}; 
+            var sumResult = SumArray.Sum(myArray);
+
+            Assert.Equal(sumResult, expectedSumResult);
+        }
+
+        [Fact]
+        public void GivenACorrectArrayTheSumVariationIsCorrectTest()
+        {
+            int expectedSumResult = 5;
+
+            int[] myArray = new int[] { 1, 1, 1, 1, 1 };
+            var sumResult = SumArray.SumVariation(myArray);
+
+            Assert.Equal(sumResult, expectedSumResult);
         }
     }
 }
