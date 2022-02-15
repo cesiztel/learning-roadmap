215d92221c7dab752f31e7f9c14f98f1b2266f9e
Cesar Izquierdo <cesarizquierdotello@C02FN212MD6M.local>
2022-02-16 00:13:04 +0100

    React: add webpack configuration to the menu app

--- 3_react/menu_app/src/App.js
+++ 3_react/menu_app/src/App.js
@@ -1,20 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from './01_Counter/Counter';
-import Greeting from "./02_Greeting/Greeting";
-import GreetingLocalStorage from "./03_GreetingsLocalStorage/GreetingLocalStorage";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "../src/data/recipes.json";
+import Menu from "../src/components/Menu";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>01 - Counter</h3>
-        <Counter />
-        <h3>02 - Greeting</h3>
-        <Greeting />
-        <h3>03 - GreetingLocalStorage</h3>
-        <GreetingLocalStorage />
+        <Menu recipes={data} title="Delicious Recipes" />
       </header>
     </div>
   );
--- 3_react/menu_app/src/index.js
+++ 3_react/menu_app/src/index.js
@@ -3,21 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import reactDom from "react-dom";
-import Menu from "../src/components/Menu";
-import data from "../src/data/recipes.json";
 
 ReactDOM.render(
-  <Menu recipes={data} title="Delicious Recipes" />,
-  document.getElementById("root")
-);
-
-/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-); */
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
