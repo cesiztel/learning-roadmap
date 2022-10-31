a654784913b7863a6cb2f0e842456e25aaccdcaf
Cesar Izquierdo Tello <cesiztel@gmail.com>
2022-10-31 23:12:57 +0100

    feat: An attempt to create a lambda router

--- aws/cdk-projects/cdk-first-project/app.py
+++ aws/cdk-projects/cdk-first-project/app.py
@@ -6,6 +6,7 @@ from cdk_first_project.cdk_first_project_stack import CdkFirstProjectStack
 
 
 app = cdk.App()
+
 CdkFirstProjectStack(app, "cdk-first-project")
 
 app.synth()
--- aws/cdk-projects/cdk-first-project/cdk_first_project/cdk_first_project_stack.py
+++ aws/cdk-projects/cdk-first-project/cdk_first_project/cdk_first_project_stack.py
@@ -4,16 +4,28 @@ from aws_cdk import (
     aws_lambda as _lambda
 )
 
-
 class CdkFirstProjectStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(
+        self, 
+        scope: Construct,
+        construct_id: str,
+        **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        my_lambda = _lambda.Function(
+        # Add powertools as lambda layer
+        powertools_layer = _lambda.LayerVersion.from_layer_version_arn(
+            self,
+            id="lambda-powertools",
+            layer_version_arn=f"arn:aws:lambda:{self.region}:017000801446:layer:AWSLambdaPowertoolsPythonV2:12"
+        )
+
+        # Create lambda and add Power tools
+        _lambda.Function(
             self,
             'HelloHandler',
             runtime=_lambda.Runtime.PYTHON_3_7,
             code=_lambda.Code.from_asset('lambda'),
-            handler='hello.handler'
+            handler='hello.handler',
+            layers=[powertools_layer]
         )
